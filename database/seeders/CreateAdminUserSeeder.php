0595caf2892539d418512ad4c537e7ffe65b12ca
samuniv <samuniv987@gmail.com>
2021-05-28 12:37:33 +0545

    initial commit

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Database\Seeders;
+
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+
+class CreateAdminUserSeeder extends Seeder
+
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+
+    {
+
+        $user = User::create([
+
+            'name' => 'Samuel Maharjan',
+
+            'email' => 'admin@gmail.com',
+
+            'password' => bcrypt('admin')
+
+        ]);
+
+
+        $role = Role::create(['name' => 'Admin']);
+
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+
+        $role->syncPermissions($permissions);
+
+
+        $user->assignRole([$role->id]);
+
+    }
+
+}
